071c68e386cca69525d98d23bb30616828b813b3
Bruno Degli Esposti <brunodegliesposti@gmail.com>
2022-07-27 16:23:22 +0200

    Fix a bug related to MPI slots

--- Codice/test2.sh
+++ Codice/test2.sh
@@ -10,7 +10,7 @@ do
 	for W in 1 8 128 1024
 	do
 		printf "Running test %d/%d...\n" $counter $ntests
-		mpirun -H A660-Ubuntu -np $nprocesses python3 07-FDM-parallel.py 40000 1e-12 $W results-test-2.txt
+		mpirun -H A660-Ubuntu:$nprocesses python3 07-FDM-parallel.py 40000 1e-12 $W results-test-2.txt
 		let counter=counter+1
 		sleep 5
 	done
@@ -19,7 +19,7 @@ done
 for W in 1 8 128 1024
 do
 	printf "Running test %d/%d...\n" $counter $ntests
-	mpirun -H A660-Ubuntu,N56VZ-Ubuntu -np 8 python3 07-FDM-parallel.py 40000 1e-12 $W results-test-2.txt
+	mpirun -H A660-Ubuntu:4,N56VZ-Ubuntu:4 python3 07-FDM-parallel.py 40000 1e-12 $W results-test-2.txt
 	let counter=counter+1
 	sleep 5
 done
